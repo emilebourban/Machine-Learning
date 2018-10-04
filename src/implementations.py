ed7c779597535903c4394bf31a2e86ebe1a70d3b
emilebourban <emilebourban@gmail.com>
2018-10-04 14:37:29 +0200

    Add files via upload

---
+++ src/implementations.py
@@ -0,0 +1,39 @@
+
+
+
+def least_square_GD(y, tx, initial_w, max_iters, gamma)
+	'''Linear regression using gradient descent'''
+	
+	
+    return (w, loss)
+
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma)
+	'''Linear regression using stochastic gradient descent'''
+	
+	
+    return (w, loss) 
+    
+        
+def least_squares(y, tx) 
+	'''Least squares regression using normal equations'''
+	
+	
+    return (w, loss)
+    
+def ridge_regression(y, tx, lambda_)
+	'''Ridge regression using normal equations'''
+    return (w, loss)
+    
+def logistic_regression(y, ty, initial_w, max_iters, gamma)
+	'''Logistic regression using gradient descent or SGD'''
+    return (w, loss)
+    
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma)
+	'''Regularized logistic regression using gradient descent
+	or SGD'''
+	
+	
+    return (w, loss)
+    
