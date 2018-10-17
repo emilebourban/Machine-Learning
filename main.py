70e84ab2eeb7b1a43167d6fabf99544310266b9c
FlorianDelberghe <florian.delberghe@outlook.com>
2018-10-17 16:26:45 +0200

    Added main.py, helpers.py and implementations.py
    main.py load and uses the data, helpers and implementations contain useful functions

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from './src/helpers.py'
+#from src import *
+# from src import implementations
+
+data_path = './all/'
+if False:
+    train_data = load_data(data_path+'train.csv')
+    test_data = load_data(data_path+'test.csv')
+
+print(train_data[1], '\n')
--- src/implementations.py
+++ src/implementations.py
@@ -1,7 +1,25 @@
+import numpy as np
+
+def compute_mse(y, tx, w):
+    '''Calculates the Mean Square Error'''
+    return 1/2 * np.mean(e**2)
 
 
+def calculate_mae(e):
+    ''' Calculates the Mean Absolute Error '''
+    return np.mean(np.abs(e))
+    
 
-def least_square_GD(y, tx, initial_w, max_iters, gamma)
+def compute_loss(y, tx, w, error='square'):
+    '''  Computes loss function, type=square/absolute'''
+    e = y - (tx @ w)
+    if type == 'absolute':
+        return calculate_mae(e)
+    else:
+        return calculate_mse(e)
+    
+
+def least_square_GD(y, tx, initial_w, max_iters, gamma):
 	'''Linear regression using gradient descent'''
 	
 	
@@ -9,28 +27,31 @@ def least_square_GD(y, tx, initial_w, max_iters, gamma)
 
 
 
-def least_squares_SGD(y, tx, initial_w, max_iters, gamma)
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
 	'''Linear regression using stochastic gradient descent'''
 	
 	
     return (w, loss) 
     
         
-def least_squares(y, tx) 
-	'''Least squares regression using normal equations'''
-	
-	
+def least_squares(y, tx):
+    """calculate the least squares solution."""
+    
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    w = np.linalg.solve(a, b)
+    loss = compute_loss(y, tx, w)
     return (w, loss)
     
-def ridge_regression(y, tx, lambda_)
+def ridge_regression(y, tx, lambda_):
 	'''Ridge regression using normal equations'''
     return (w, loss)
     
-def logistic_regression(y, ty, initial_w, max_iters, gamma)
+def logistic_regression(y, ty, initial_w, max_iters, gamma):
 	'''Logistic regression using gradient descent or SGD'''
     return (w, loss)
     
-def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma)
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
 	'''Regularized logistic regression using gradient descent
 	or SGD'''
 	
