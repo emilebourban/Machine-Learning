1300533ed679a17da5f18b67c1c9f1df8d146ea3
FlorianDelberghe <florian.delberghe@outook.com>
2018-10-18 07:56:32 +0200

    Can now load data

--- main.py
+++ main.py
@@ -2,46 +2,65 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from src import helpers, implementations
+from src import implementations as imp
+from src import proj1_helpers as help1
 
 data_path = './all/'
 reload_data = False
-path_dataset = data_path+r'train.csv'
-data = []
+all_cols = True
+under_40 = True
+under_20 = True
+under_10 = True
 
 if reload_data:
-    data, y = helpers.load_data(data_path+r'train.csv')
-#    test_data, te_col = load_data(data_path+r'test.csv')
+    yb, input_data, ids = help1.load_csv_data(data_path+r'train.csv')
+    yb_te, input_data_te, ids_te = help1.load_csv_data(data_path+r'test.csv')
 
-#data = np.genfromtxt(path_dataset, delimiter=",", 
-#                              usecols=range(2, 32), skip_header=1)
-#
-#y = np.genfromtxt(path_dataset, delimiter=',', usecols=1, 
-#                      dtype=str, skip_header=1)
+
+percent_nan = []
+for i in range(input_data.shape[1]):
+    c = 0
+    for j in range(input_data.shape[0]):    
+        if (input_data[j][i] == 0):
+            c +=1
+    percent_nan = np.r_[percent_nan, (c * 100)/input_data.shape[0]]       
+    print(percent_nan[i], "% wrong values in col:", i)
     
     
-
-
-y, data, cols = helpers.load_data(path_dataset)
-
-
-print(data.shape)
-print(type(data), type(data[1]), '\n')
-
-print(y.shape)
-print(type(y), type(y[1]), '\n')
-
-print(cols)
-
+if all_cols:
+    w, loss = imp.least_squares(yb, input_data)
+    print("Taking all colomns", '\n w* = \n', w, '\n loss =', loss)
+    print("loss test =", imp.compute_loss(yb_te, input_data_te, w))
+        
+if under_40:
+    data = np.delete(input_data, [4,5,6,12,26,27,28,29], axis=1)
+    data_te = np.delete(input_data_te, [4,5,6,12,26,27,28,29], axis=1)
+    w2, loss2 = imp.least_squares(yb, data)
+    print("<40% wrong values", '\n w* = \n', w2, '\n loss =', loss2)
+    print("loss test =", imp.compute_loss(yb_te, data_te, w2))
     
+    
+if under_20:
+    data = np.delete(input_data, [4,5,6,12,22,23,24,25,26,27,28,29], axis=1)
+    data_te = np.delete(input_data_te, [4,5,6,12,22,23,24,25,26,27,28,29], axis=1)
+    w3, loss3 = imp.least_squares(yb, data)
+    print("<20% wrong values", '\n w* = \n', w3, '\n loss =', loss3)
+    print("loss test =", imp.compute_loss(yb_te, data_te, w3))
 
-
-
-
-
-
-
-
+if under_10:
+    data = np.delete(input_data, [0,4,5,6,12,22,23,24,25,26,27,28,29], axis=1)
+    data_te = np.delete(input_data_te, [0,4,5,6,12,22,23,24,25,26,27,28,29], axis=1)
+    w3, loss3 = imp.least_squares(yb, data)
+    print("<10% wrong values", '\n w* = \n', w3, '\n loss =', loss3)
+    print("loss test =", imp.compute_loss(yb_te, data_te, w3))
+    
+if under_10:
+    data = np.delete(input_data, [0,4,5,6,12,22,23,24,25,26,27,28,29], axis=1)
+    data_te = np.delete(input_data_te, [0,4,5,6,12,22,23,24,25,26,27,28,29], axis=1)
+    np.append(data, np.ones(data[0].shape[0]))
+    w3, loss3 = imp.least_squares(yb, data)
+    print("<10% wrong values  w/ offset", '\n w* = \n', w3, '\n loss =', loss3)
+    print("loss test =", imp.compute_loss(yb_te, data_te, w3))
 
 
 
--- src/implementations.py
+++ src/implementations.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-def compute_mse(y, tx, w):
+def calculate_mse(e):
     '''Calculates the Mean Square Error'''
     return 1/2 * np.mean(e**2)
-
 
 def calculate_mae(e):
     ''' Calculates the Mean Absolute Error '''
@@ -19,18 +18,36 @@ def compute_loss(y, tx, w, error='square'):
         return calculate_mse(e)
     
 
-#def least_square_GD(y, tx, initial_w, max_iters, gamma):
-#	'''Linear regression using gradient descent'''	
-#    
-#    return (w, loss)
-#   
-#
-#
-#
-#def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
-#	'''Linear regression using stochastic gradient descent'''
-#	
-#   return (w, loss) 
+def least_square_GD(y, tx, initial_w, max_iters, gamma):
+    '''Linear regression using gradient descent'''	
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute loss, gradient
+        grad, err = compute_gradient(y, tx, w)
+        loss = calculate_mse(err)
+        # gradient w by descent update
+        w = w - gamma * grad
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return ws[-1], loss[-1]
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    print(y.shape, tx.shape, w.shape)
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    '''Linear regression using stochastic gradient descent'''
+    return (w, loss) 
     
         
 def least_squares(y, tx):
@@ -40,7 +57,7 @@ def least_squares(y, tx):
     b = tx.T.dot(y)
     w = np.linalg.solve(a, b)
     loss = compute_loss(y, tx, w)
-    return (w, loss)
+    return w, loss
     
 #def ridge_regression(y, tx, lambda_):
 #	'''Ridge regression using normal equations'''
@@ -49,11 +66,10 @@ def least_squares(y, tx):
 #def logistic_regression(y, ty, initial_w, max_iters, gamma):
 #	'''Logistic regression using gradient descent or SGD'''
 #    return (w, loss)
-    
+#    
 #def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
 #	'''Regularized logistic regression using gradient descent
 #	or SGD'''
 #	
 #	
 #    return (w, loss)
-    
