74b8a6f39ce3447840b386013ad0ad4c98caedc1
Tkastylevsky <43445479+Tkastylevsky@users.noreply.github.com>
2018-10-18 15:05:03 +0200

    Update implementations.py

--- src/implementations.py
+++ src/implementations.py
@@ -198,4 +198,25 @@ def cross_validation_visualization(lambds, mse_tr, mse_te):
     plt.savefig("cross_validation")
 
 
+def princomp(A):
+ """ performs principal components analysis 
+     (PCA) on the n-by-p data matrix A
+     Rows of A correspond to observations, columns to variables. 
 
+ Returns :  
+  coeff :
+    is a p-by-p matrix, each column containing coefficients 
+    for one principal component.
+  score : 
+    the principal component scores; that is, the representation 
+    of A in the principal component space. Rows of SCORE 
+    correspond to observations, columns to components.
+  latent : 
+    a vector containing the eigenvalues 
+    of the covariance matrix of A.
+ """
+ # computing eigenvalues and eigenvectors of covariance matrix
+ M = (A-np.mean(A.T,axis=1)).T # subtract the mean (along columns)
+ [latent,coeff] = np.linalg.eig(np.cov(M)) # attention:not always sorted
+ score = np.dot(coeff.T,M) # projection of the data in the new space
+ return coeff,score,latent
