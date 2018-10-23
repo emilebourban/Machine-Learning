31dab6b05edcbfd3fecd24e4208f0ccc2ac19e50
emilebourban <emilebourban@gmail.com>
2018-10-23 06:53:05 +0200

    Update implementations.py
    added least_square_GD and least_square_SGD

--- src/implementations.py
+++ src/implementations.py
@@ -20,12 +20,9 @@ def compute_loss(y, tx, w, error='square'):
     else:
         return calculate_mse(e)
     
-# TODO
 def least_square_GD(y, tx, initial_w, max_iters, gamma):
     '''Linear regression using gradient descent'''	
     # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
     w = initial_w
     for n_iter in range(max_iters):
         # compute loss, gradient
@@ -34,24 +31,50 @@ def least_square_GD(y, tx, initial_w, max_iters, gamma):
         # gradient w by descent update
         w = w - gamma * grad
         # store w and loss
-        ws.append(w)
-        losses.append(loss)
-        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-
-    return ws[-1], loss[-1]
+    return w, loss
 
 def compute_gradient(y, tx, w):
     """Compute the gradient."""
-    print(y.shape, tx.shape, w.shape)
     err = y - tx.dot(w)
     grad = -tx.T.dot(err) / len(err)
     return grad, err
 
-def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma, batch_size):
     '''Linear regression using stochastic gradient descent'''
+    w = initial_w 
+    for i in range(max_iters):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
+            grad = compute_gradient(minibatch_y, minibatch_tx, w)[0]
+            w = w - gamma*grad
+        loss = compute_loss(y, tx, w)
     return (w, loss) 
-    
         
 def least_squares(y, tx):
     """calculate the least squares solution."""
@@ -63,7 +86,7 @@ def least_squares(y, tx):
     
 def ridge_regression(y, tx, lambda_):
     '''Ridge regression using normal equations'''
-    A = tx.T.dot(tx) - lambda_ * 2 * tx.shape[0] * np.identity(tx.shape[1])
+    A = tx.T.dot(tx) - (2*tx.shape[0]*lambda_)*np.identity(tx.shape[1])
     B = tx.T.dot(y) 
     w = np.linalg.solve(A, B)   
     loss = compute_loss(y, tx, w)
