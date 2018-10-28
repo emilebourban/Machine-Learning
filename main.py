1f2ebf34615aef90a0d3344ac639ca7310f82515
emilebourban <emilebourban@gmail.com>
2018-10-28 16:39:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -75,56 +75,44 @@ if LOG_REGRESS:
         
 #%%
 if REG_LOG_REGRESS:
-    
-
-    gammas = np.logspace(-3, 2, 10)
-    lambdas = np.logspace(-10, 1, 4)
+    gammas = np.logspace(-7, 0, 2)
+    lambdas = np.logspace(-7, 0, 3)
     max_iter = 100
+    threshold = 1e-8
     
-    #creates a 3d array that stores the accuracy for each degree, gamma and lambda
-    accuracy_poly_log_reg = np.zeros((len(DEGREE), gammas.shape[0], lambdas.shape[0]))
-    
-    for d in range(len(DEGREE)): 
-        data_poly = imp.build_poly(data, DEGREE[d])
-        y_tr, y_te, data_poly_tr, data_poly_te = help1.split_data(Y_IN, data_poly, K_FOLD)
-        initial_w = np.ones(data_poly_tr.shape[2])
-        ws_poly_log_reg = np.zeros((K_FOLD, data_poly_tr.shape[2])); losses_poly_log_reg = [];
-        for g in range(gammas.shape[0]):
-            for l in range(lambdas.shape[0]):
-                for k in range(K_FOLD):
-                    w_temp, loss_temp = imp.logistic_regression(y_tr[k], data_poly_tr[k], initial_w, max_iter, gammas[g])
-                    w_temp, loss_temp = imp.reg_logistic_regression(y_tr[k], data_poly_tr[k], lambdas[l], initial_w, max_iter, gammas[g])
-                    ws_poly_log_reg[k] = np.array(w_temp)
-                    losses_poly_log_reg.append(loss_temp)
-                w_poly_log_reg_mean = np.array(ws_poly_log_reg.mean(axis=0))
-                accuracy = []
-                for k in range(K_FOLD):
-                    accuracy.append(imp.calculate_accuracy(data_poly_te[k], y_te[k], w_poly_log_reg_mean))
-                accuracy_poly_log_reg[d, g, l] = np.array(accuracy).mean()
-                print('Accuracy = {:.4}; Degree = {}; Lambda = {:.3E}; Gamma = {:.3E}; Lambda*Gamma = {:.3} '.format(
-                      np.array(accuracy).mean(), DEGREE[d], lambdas[l], gammas[g], lambdas[l]*gammas[g]))
+    data_poly = imp.build_poly(data, 3)
+    y_tr, y_te, data_poly_tr, data_poly_te = help1.split_data(Y_IN, data_poly, K_FOLD)
+    #creat a grid containing accuracy for each lambda and gamma combination
+    accuracy_grid = np.zeros((gammas.shape[0], lambdas.shape[0]))
+    step = 0
+    for g in range(gammas.shape[0]):    
+        for l in range(lambdas.shape[0]):
+            ws = np.ones((data_poly_tr.shape[0], data_poly_tr.shape[2]))
+            for k in range(K_FOLD):
+                losses = []
+                for i in range(max_iter):
+                    loss, w = imp.learning_by_penalized_gradient(y_tr[k], data_poly_tr[k], ws[k], gammas[g], lambdas[l])
+                    ws[k] = w
+                    losses.append(loss)
+            step += 1
+            print('step:' + str(step) + '/' + str(gammas.shape[0]*lambdas.shape[0]))
+            accuracy = []
+            for k in range(K_FOLD):
+                accuracy.append(imp.calculate_accuracy(
+                        data_poly_te[k], y_te[k], np.array(ws.mean(axis=0))))
+            accuracy_grid[g,l] = np.array(accuracy).mean()
             
-    
-    temp_ind = np.where(accuracy_poly_log_reg == accuracy_poly_log_reg.max())
-    best_acc_ind = (temp_ind[0][0],temp_ind[1][0], temp_ind[2][0])
-    best_acc = accuracy_poly_log_reg[best_acc_ind]
-    best_gamma = gammas[best_acc_ind[1]]
-    best_lambda = lambdas[best_acc_ind[2]]
-    best_deg = DEGREE[best_acc_ind[0]]
-    best_w, _ = imp.reg_logistic_regression(y_tr[k], data_poly_tr[k], lambdas[l], initial_w, max_iter, gammas[g])
-    print('Best accuracy = {}; Degree = {}; Gamma = {}; Lambda {}'.format(\
-            best_acc, best_deg, best_gamma, best_lambda))
-    
-    #generate figure of accuracy depending on gamma values for different polynomial degrees
-    fig7 = plt.figure(7)
-    ax7 = fig7.add_subplot(111)
-    ax7.set_xscale('log')
-    for g in range(len(gammas)):
-        plt.plot(lambdas, accuracy_poly_log_reg[3,g,:], label='Accuracy w/ gamma: '+str(gammas[g])) 
+   #generate figure of accuracy depending on gamma values
+    fig_reglog = plt.figure(1)
+    ax_reglog = fig_reglog.add_subplot(111)
+    ax_reglog.set_xscale('log')
+    for g in range(gammas.shape[0]):
+        plt.plot(lambdas, accuracy_grid[g], label='Accuracy w/ gamma: '+str(gammas[g])) 
     plt.title('Accuracy for logistic regression')
     plt.xlabel('gamma'); plt.ylabel('Accuracy');
     plt.legend(loc=0)
     plt.show()
+    
 #%%
     
 #Ridge regression with poly for different lambdas
